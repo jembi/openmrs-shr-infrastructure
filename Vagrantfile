b943fa7ceeda1bb5b95a1084a81187411b12ccd6
Pascal Brandt <psbrandt@gmail.com>
2015-06-24 15:00:55 +0200

    Updated OpenMRS to 1.12.x.

--- Vagrantfile
+++ Vagrantfile
@@ -1,19 +1,28 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-Vagrant::Config.run do |config|
-  config.vm.box = "precise32"
-  config.vm.box_url = "http://files.vagrantup.com/precise32.box"
-  config.vm.customize ["modifyvm", :id, "--memory", 1536]
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
 
-  config.vm.network :bridged
-  config.vm.forward_port 8080, 8082
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
+  # Use trusty64 base box
+  config.vm.box = "ubuntu/trusty64"
+  config.vm.provider "virtualbox" do |v|
+    v.customize ["modifyvm", :id, "--memory", 1536]
+  end
+
+  # Configure port forwarding
+  config.vm.network "forwarded_port", guest: 8080, host: 8082
+
+  # Ensure packages are up to date
   config.vm.provision :shell do |shell|
     # Uncomment the line below if you're in the Jembi Cape Town office
-    #shell.inline = "echo \"Acquire::http::Proxy \\\"http://192.168.1.53:3142\\\";\" > /etc/apt/apt.conf.d/01JembiServerproxy"
-    shell.inline = "mkdir -p /etc/puppet/modules;
-      puppet module install puppetlabs/vcsrepo"
+    shell.inline = "echo \"Acquire::http::Proxy \\\"http://192.168.1.53:3142\\\";\nAcquire::http::Proxy { download.oracle.com DIRECT; }\" > /etc/apt/apt.conf.d/01JembiServerproxy;
+      sudo add-apt-repository ppa:webupd8team/java -y;
+      sudo apt-get update"
+    # shell.inline = "sudo add-apt-repository ppa:webupd8team/java -y \
+    #   sudo apt-get updatesudo apt-get update";
   end
 
   config.vm.provision :puppet do |puppet|
--- manifests/openmrs-shr.pp
+++ manifests/openmrs-shr.pp
@@ -1,86 +1,46 @@
 $mysql_password = "shr"
 
-# defaults for Exec
+# Defaults for Exec
 Exec {
   path => ["/bin", "/sbin", "/usr/bin", "/usr/sbin", "/usr/local/bin", "/usr/local/sbin"],
   user => 'root',
 }
 
-# Make sure package index is updated (when referenced by require)
-exec { "apt-get update":
-  command => "apt-get update",
-  user => "root",
+# Install MySQL
+package { "mysql-server":
+  ensure => latest
 }
 
-# Install these packages after apt-get update
-define apt::package($ensure='latest') {
-  package { $name:
-  ensure => $ensure,
-  require => Exec['apt-get update'];
-  }
+# Install Java 8
+exec { "install-java8":
+  command => "echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections; sudo apt-get install oracle-java8-installer -y -q",
+  timeout => 0
 }
 
-apt::package { "mysql-server": ensure => latest }
-apt::package { "openjdk-7-jdk": ensure => latest }
-# apt::package { "maven": ensure => latest }
-apt::package { "tomcat6": ensure => latest }
-# apt::package { "git": ensure => latest }
-apt::package { "vim": ensure => latest }
+exec { "set-java8-default":
+  command => "sudo apt-get install oracle-java8-set-default",
+  timeout => 0,
+  require => Exec["install-java8"]
+}
 
+# Install Tomcat 7
+package { "tomcat7":
+  ensure => latest,
+  require => Exec["set-java8-default"]
+}
+
+# Fix Tomcat 7 to work with Java 8
+exec { "fix-tomcat7":
+  command => "sudo sed -i 's/\\(JDK_DIRS=.*\\)\"/\\1\\ \\/usr\\/lib\\/jvm\\/java\\-8\\-oracle\"/g' /etc/init.d/tomcat7",
+  timeout => 0,
+  require => Package["tomcat7"],
+  notify => Service["tomcat7"]
+}
 
 # Fetch modules
-
-# define shr::module($depends="") {
-#   vcsrepo { "/home/vagrant/${name}":
-#     ensure => present,
-#     provider => git,
-#     source => "git://github.com/jembi/${name}.git",
-#     revision => "master",
-#     require => Package["git"]
-#   }
-
-#   exec { "setup-${name}-dir-permissions":
-#     cwd => "/home/vagrant",
-#     command => "chown -R vagrant:vagrant ${name}",
-#     require => Vcsrepo["/home/vagrant/${name}"]
-#   }
-
-#   if $depends == "" {
-#     exec { "${name}":
-#       cwd => "/home/vagrant/${name}",
-#       command => "mvn install -DskipTests=true",
-#       timeout => 0,
-#       user => "vagrant",
-#       require => [ Package["openjdk-6-jdk"], Package["maven"],
-#         Exec["setup-${name}-dir-permissions"] ]
-#     }
-#   } else {
-#     exec { "${name}":
-#       cwd => "/home/vagrant/${name}",
-#       command => "mvn install -DskipTests=true",
-#       timeout => 0,
-#       user => "vagrant",
-#       require => [ Package["openjdk-6-jdk"], Package["maven"],
-#         Exec["setup-${name}-dir-permissions"], Exec["${depends}"] ]
-#     }
-#   }
-# }
-
-# shr::module { "openmrs-module-shr-cdahandler": }
-# shr::module { "openmrs-module-shr-unstructureddata": }
-# shr::module { "openmrs-module-shr-contenthandler": }
-# shr::module { "openmrs-module-shr-rest": depends => "openmrs-module-shr-contenthandler" }
-
-# $cdaOmod = "/home/vagrant/openmrs-module-shr-cdahandler/omod/target/*.omod"
-# $udOmod = "/home/vagrant/openmrs-module-shr-unstructureddata/omod/target/*.omod"
-# $chOmod = "/home/vagrant/openmrs-module-shr-contenthandler/omod/target/*.omod"
-# $restOmod = "/home/vagrant/openmrs-module-shr-rest/omod/target/*.omod"
-
-
-
 exec { "fetch-xds-b-module":
-  command => "wget -P /vagrant/artifacts/ https://github.com/jembi/openmrs-module-shr-xds-b-repository/releases/download/v0.4.5/xds-b-repository-0.4.5.omod",
-  creates => "/vagrant/artifacts/xds-b-repository-0.4.5.omod",
+  command => "wget -P /vagrant/artifacts/ https://s3.amazonaws.com/openshr/xds-b-repository-0.4.6-SNAPSHOT.omod",
+  creates => "/vagrant/artifacts/xds-b-repository-0.4.6-SNAPSHOT.omod",
   timeout => 0
 }
 
@@ -115,14 +75,13 @@ exec { "fetch-atna-module":
 }
 
 exec { "copy-modules":
-  command => "cp -f /vagrant/artifacts/*.omod /usr/share/tomcat6/.OpenMRS/modules/",
-  user => "tomcat6",
+  command => "cp -f /vagrant/artifacts/*.omod /usr/share/tomcat7/.OpenMRS/modules/",
+  user => "tomcat7",
   require => [ Exec["setup-openmrs-dir-permissions"], Exec["fetch-xds-b-module"], Exec["fetch-webservices-module"],
   Exec["fetch-contenthandler-module"], Exec["fetch-odd-module"], Exec["fetch-cdahandler-module"], Exec["fetch-atna-module"] ]
 }
 
 # Install OpenMRS
-
 exec { "fetch-openmrs-war":
   command => "wget -O /vagrant/artifacts/openmrs.war http://sourceforge.net/projects/openmrs/files/releases/OpenMRS_1.9.7/openmrs.war/download",
   creates => "/vagrant/artifacts/openmrs.war",
@@ -130,12 +89,17 @@ exec { "fetch-openmrs-war":
 }
 
 exec { "copy-webapp":
-  command => "cp /vagrant/artifacts/openmrs.war /var/lib/tomcat6/webapps/",
-  require => [ Package["tomcat6"], Exec["fetch-openmrs-war"], Exec["apply-db-dump"],
+  command => "cp /vagrant/artifacts/openmrs.war /var/lib/tomcat7/webapps/",
+  require => [ Package["tomcat7"], Exec["fetch-openmrs-war"], Exec["apply-db-dump"],
     Exec["openmrs-user-privileges"], Exec["copy-modules"] ]
 }
 
 # Initialise Database
+exec { "fetch-database":
+  command => "wget -P /vagrant/artifacts/ https://s3.amazonaws.com/openshr/openmrs.sql.gz",
+  creates => "/vagrant/artifacts/openmrs.sql.gz",
+  timeout => 0
+}
 
 service { "mysql":
   enable => true,
@@ -156,6 +120,7 @@ exec { "openmrs-user-password":
 
 exec { "gunzip-db-dump":
   command => "gunzip -c /vagrant/artifacts/openmrs.sql.gz > /tmp/openmrs.sql",
+  require => Exec["fetch-database"]
 }
 
 exec { "create-openmrs-db":
@@ -175,38 +140,31 @@ exec { "apply-db-dump":
 }
 
 # Tomcat and OpenMRS app configuration
-
-# exec { "copy-catalina-sh":
-#   command => "cp /vagrant/artifacts/catalina.sh /usr/share/tomcat6/bin/",
-#   require => Package["tomcat6"]
-# }
-
 exec { "setup-openmrs-conf":
-  cwd => "/usr/share/tomcat6",
+  cwd => "/usr/share/tomcat7",
   command => "tar -xzf /vagrant/artifacts/openmrs-conf-dir.tar.gz",
-  require => Package["tomcat6"]
+  require => Package["tomcat7"]
 }
 
 exec { "setup-openmrs-dir-permissions":
-  cwd => "/usr/share/tomcat6",
-  command => "chown -R tomcat6:tomcat6 .OpenMRS",
+  cwd => "/usr/share/tomcat7",
+  command => "chown -R tomcat7:tomcat7 .OpenMRS",
   require => Exec["setup-openmrs-conf"]
 }
 
-# Configure Tomcat memory
-
 # Define Tomcat service
-service { "tomcat6":
+service { "tomcat7":
     ensure  => "running",
     enable  => "true",
-    require => Package["tomcat6"],
+    require => Package["tomcat7"],
 }
 
-file { "/usr/share/tomcat6/bin/setenv.sh":
+# Configure Tomcat memory
+file { "/usr/share/tomcat7/bin/setenv.sh":
   source  => "/vagrant/artifacts/setenv.sh",
-  owner => "tomcat6",
-  group   => "tomcat6",
+  owner => "tomcat7",
+  group   => "tomcat7",
   mode  => "a+x",
-  require => Package["tomcat6"],
-  notify  => Service["tomcat6"]
+  require => Package["tomcat7"],
+  notify  => Service["tomcat7"]
 }
